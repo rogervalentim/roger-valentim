54600130484e7c5f00eea6d8006329d6d4700fe6
rogervalentim <valentim.roger33@gmail.com>
2024-04-14 20:33:49 -0300

    feat: add Skills component

---
+++ src/components/skills.tsx
@@ -0,0 +1,33 @@
+import { skills } from "../constants";
+import { CardSkills } from "./card-skills";
+import { motion } from "framer-motion";
+
+export function Skills() {
+  const container = {
+    hidden: {},
+    visible: {
+        transition: { staggerChildren: 0.2 }
+    }
+}
+
+  return (
+    <>
+    <h1 className="text-center font-bold text-3xl lg:text-4xl underline mt-10">Tecnologias</h1>
+    <section className="flex justify-around">
+     <motion.div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5  gap-4 mt-6" 
+       initial="hidden"
+       whileInView="visible"
+       viewport={{ once: true, amount: 0.5 }}
+       variants={container}>
+    {skills.map(skill => (
+       <CardSkills 
+        key={skill.name}
+        name={skill.name}
+        icon={skill.icon}
+       />
+       ))}
+    </motion.div>
+    </section>
+    </>
+  );
+}
