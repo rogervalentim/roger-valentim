e28326442389bf4be94d3b7065fe3c2e253c8791
rogervalentim <valentim.roger33@gmail.com>
2024-04-14 17:21:45 -0300

    feat: add navLinks

---
+++ src/constants/index.ts
@@ -0,0 +1,18 @@
+export const navLinks = [
+    {
+        id: 1,
+        link: "Inicio"
+    },
+    {
+        id: 2,
+        link: "Sobre"
+    },
+    {
+        id: 3,
+        link: "Tecnologias"
+    },
+    {
+        id: 4,
+        link: "Projetos"
+    }
+]
