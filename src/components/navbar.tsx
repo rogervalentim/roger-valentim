4b3506365817b017cd7e2e37e62f314e7abf1bee
rogervalentim <valentim.roger33@gmail.com>
2024-04-14 20:35:45 -0300

    feat: add better design to Navbar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -18,14 +18,14 @@ export function Navbar() {
   return (
     <>
     <nav className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="px-10 h-14 flex justify-between border-b border-border/40 items-center flex-row">
+      <div className="px-10 lg:px-16 h-14 flex justify-between border-b border-border/40 items-center flex-row">
         <div className="flex items-center gap-2 cursor-pointer">
           <FaCode className="size-6 fill-chetwode-blue-500 hover:fill-chetwode-blue-600" />
         <h1 className="text-chetwode-blue-500 font-bold text-lg">Roger</h1>
         </div>
        <ul className="hidden lg:flex lg:gap-5"> 
         {navLinks.map(item => (
-          <li className="text-muted-foreground text-lg hover:text-white cursor-pointer" key={item.id}>
+          <li className="text-muted-foreground font-semibold text-lg hover:text-white cursor-pointer" key={item.id}>
             {item.link}
           </li>
         ))}
@@ -46,7 +46,7 @@ export function Navbar() {
       <div className="z-auto absolute border-b border-border/40 flex  w-full h-screen bg-popover">
       <ul className="flex  flex-col items-center space-y-4 w-full mt-2 px-10">
       {navLinks.map(item => (
-      <li className="text-muted-foreground text-lg cursor-pointer " key={item.id}>
+      <li className="text-muted-foreground font-semibold text-lg cursor-pointer " key={item.id}>
         {item.link}
       </li>
       ))}
