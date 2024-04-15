249b3c86db2843f07691b0cef4e50b5e0e16bc4d
rogervalentim <valentim.roger33@gmail.com>
2024-04-15 20:07:13 -0300

    feat: add Footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+export function Footer() {
+  const date = new Date();
+
+  const year = date.getFullYear();
+  return (
+    <footer className="flex justify-center items-center border-t border-border h-14 mt-10">
+      <p className="text-muted-foreground hover:text-white">
+        Roger valentim &copy;{year}
+      </p>
+    </footer>
+  );
+}
