c7c10cc8988a94506ab9608db532507509a96851
rogervalentim <valentim.roger33@gmail.com>
2024-05-10 21:37:04 -0300

    feat: add socialmedia to footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,12 +1,17 @@
+import { SocialMedia } from "../home/social-media";
+
 export function Footer() {
   const date = new Date();
 
   const year = date.getFullYear();
   return (
-    <footer className="flex justify-center items-center border-t border-border/40 h-14 mt-10">
+    <footer className="flex justify-around items-center border-t border-border/40 h-14 mt-10">
       <p className="text-muted-foreground hover:text-white">
         Roger valentim &copy;{year}
       </p>
+      <div>
+        <SocialMedia />
+      </div>
     </footer>
   );
 }
