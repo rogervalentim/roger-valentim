99adcb8e3d19a9406082f5e0dbd2b5de1dd53515
rogervalentim <valentim.roger33@gmail.com>
2024-07-01 16:31:58 -0300

    feat: add github profile project

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,13 +1,27 @@
-import { FaHtml5, FaCss3Alt, FaReact, FaGithub, FaGitAlt } from "react-icons/fa";
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaReact,
+  FaGithub,
+  FaGitAlt
+} from "react-icons/fa";
 import { RiJavascriptFill } from "react-icons/ri";
 import { BiLogoTypescript, BiLogoTailwindCss } from "react-icons/bi";
-import { SiNextdotjs, SiJest, SiStyledcomponents, SiPrisma, SiZod, SiReacthookform, SiReactquery  } from "react-icons/si";
+import {
+  SiNextdotjs,
+  SiJest,
+  SiStyledcomponents,
+  SiPrisma,
+  SiZod,
+  SiReacthookform,
+  SiReactquery
+} from "react-icons/si";
 import rogerCine from "../assets/projects/roger-cine-new.jpg";
 import mksSistemas from "../assets/projects/mks-sistemas.jpg";
-import carHub from "../assets/projects/carhub.jpg";
 import gym from "../assets/projects/gym.jpg";
 import ifood from "../assets/projects/ifood.png";
 import auth from "../assets/projects/auth-portal.jpg";
+import github from "../assets/projects/github-profile.png";
 import certificate1 from "../assets/certificates/certificado-fundamentos.png";
 import certificate2 from "../assets/certificates/certificado-dh.jpg";
 import certificate3 from "../assets/certificates/certificado-react.png";
@@ -15,30 +29,30 @@ import certificate4 from "../assets/certificates/certificado-freecodecamp.png";
 import certificate5 from "../assets/certificates/certificado-imersao-alura.png";
 
 export const images = {
-    FaHtml5,
-    FaCss3Alt,
-    RiJavascriptFill,
-    BiLogoTypescript,
-    BiLogoTailwindCss,
-    FaReact,
-    SiNextdotjs,
-    SiJest,
-    SiZod,
-    SiReacthookform,
-    SiReactquery,
-    FaGithub,
-    FaGitAlt,
-    SiStyledcomponents,
-    SiPrisma,
-    rogerCine,
-    mksSistemas,
-    carHub,
-    gym,
-    ifood,
-    auth,
-    certificate1,
-    certificate2,
-    certificate3,
-    certificate4,
-    certificate5,
-}
+  FaHtml5,
+  FaCss3Alt,
+  RiJavascriptFill,
+  BiLogoTypescript,
+  BiLogoTailwindCss,
+  FaReact,
+  SiNextdotjs,
+  SiJest,
+  SiZod,
+  SiReacthookform,
+  SiReactquery,
+  FaGithub,
+  FaGitAlt,
+  SiStyledcomponents,
+  SiPrisma,
+  rogerCine,
+  mksSistemas,
+  gym,
+  ifood,
+  github,
+  auth,
+  certificate1,
+  certificate2,
+  certificate3,
+  certificate4,
+  certificate5
+};
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,157 +1,193 @@
-import { images } from "./images"
+import { images } from "./images";
 
 export const navLinks = [
-    {
-        id: 1,
-        link: "Inicio"
-    },
-    {
-        id: 2,
-        link: "Sobre"
-    },
-    {
-        id: 3,
-        link: "Tecnologias"
-    },
-    {
-        id: 4,
-        link: "Projetos"
-    },
-    {
-        id: 5,
-        link: "Certificados"
-    }
-]
+  {
+    id: 1,
+    link: "Inicio"
+  },
+  {
+    id: 2,
+    link: "Sobre"
+  },
+  {
+    id: 3,
+    link: "Tecnologias"
+  },
+  {
+    id: 4,
+    link: "Projetos"
+  },
+  {
+    id: 5,
+    link: "Certificados"
+  }
+];
 
 export const skills = [
-    {
-        id: 1,
-        name: "HTML",
-        icon:  images.FaHtml5,
-    },
-    {
-        id: 2,
-        name: "CSS",
-        icon:  images.FaCss3Alt,
-    },
-    {
-        id: 3,
-        name: "JAVASCRIPT",
-        icon:  images.RiJavascriptFill,
-    },
-    {
-        id: 4,
-        name: "TYPESCRIPT",
-        icon:  images.BiLogoTypescript,
-    },
-    {
-        id: 5,
-        name: "TAILWINDCSS",
-        icon:  images.BiLogoTailwindCss,
-    },
-    {
-        id: 6,
-        name: "React",
-        icon:  images.FaReact,
-    },
-    {
-        id: 7,
-        name: "NEXT JS",
-        icon:  images.SiNextdotjs,
-    },
-    {
-        id: 8,
-        name: "JEST",
-        icon:  images.SiJest,
-    },
-    {
-        id: 9,
-        name: "GITHUB",
-        icon:  images.FaGithub,
-    },
-    {
-        id: 10,
-        name: "GIT",
-        icon:  images.FaGitAlt,
-    },
-]
+  {
+    id: 1,
+    name: "HTML",
+    icon: images.FaHtml5
+  },
+  {
+    id: 2,
+    name: "CSS",
+    icon: images.FaCss3Alt
+  },
+  {
+    id: 3,
+    name: "JAVASCRIPT",
+    icon: images.RiJavascriptFill
+  },
+  {
+    id: 4,
+    name: "TYPESCRIPT",
+    icon: images.BiLogoTypescript
+  },
+  {
+    id: 5,
+    name: "TAILWINDCSS",
+    icon: images.BiLogoTailwindCss
+  },
+  {
+    id: 6,
+    name: "React",
+    icon: images.FaReact
+  },
+  {
+    id: 7,
+    name: "NEXT JS",
+    icon: images.SiNextdotjs
+  },
+  {
+    id: 8,
+    name: "JEST",
+    icon: images.SiJest
+  },
+  {
+    id: 9,
+    name: "GITHUB",
+    icon: images.FaGithub
+  },
+  {
+    id: 10,
+    name: "GIT",
+    icon: images.FaGitAlt
+  }
+];
 
 export const projectsData = [
-    {
-        id: 1,
-        name: "Roger cine",
-        description: "O Roger Cine é um portal interativo projetado para facilitar a descoberta de filmes, séries e informações sobre atores. Com uma interface intuitiva e recursos de busca avançados, os usuários podem encontrar facilmente o conteúdo de entretenimento que amam.",
-        technologies: [images.FaReact, images.BiLogoTailwindCss, images.BiLogoTypescript, images.FaGithub],
-        image: images.rogerCine,
-        deploy: "https://roger-cines.netlify.app/",
-        repository: "https://github.com/rogervalentim/movies"
-    },
-    {
-        id: 2,
-        name: "Ifood",
-        description: "O Mks Sistemas é um projeto técnico que demonstra a implementação de um carrinho de compras interativo e moderno. Utilizando as melhores práticas e tecnologias atuais, o site oferece uma experiência de usuário otimizada e um fluxo de compra simplificado.",
-        technologies: [images.SiNextdotjs, images.BiLogoTailwindCss, images.BiLogoTypescript, images.SiPrisma, images.FaGithub],
-        image: images.ifood,
-        deploy: "https://ifood-fsw.vercel.app/",
-        repository: "https://github.com/rogervalentim/ifood-fsw"
-    },
-    {
-        id: 3,
-        name: "Auth Portal",
-        description: "Este projeto consiste em um sistema de autenticação de usuários com três telas principais: Registro, Autenticação e Dashboard. O sistema se comunica com uma API para realizar as operações necessárias de registro, autenticação, exibição de usuários e logout.",
-        technologies: [images.FaReact, images.BiLogoTailwindCss, images.BiLogoTypescript, images.SiReactquery, images.SiReacthookform, images.SiZod, images.FaGithub],
-        image: images.auth,
-        deploy: "https://auth-portal-taupe.vercel.app/",
-        repository: "https://github.com/rogervalentim/auth-portal"
-    },
-    {
-        id: 4,
-        name: "Mks Sistemas",
-        description: "O Mks Sistemas é um projeto técnico que demonstra a implementação de um carrinho de compras interativo e moderno. Utilizando as melhores práticas e tecnologias atuais, o site oferece uma experiência de usuário otimizada e um fluxo de compra simplificado.",
-        technologies: [images.SiNextdotjs, images.SiStyledcomponents, images.BiLogoTypescript, images.SiJest, images.FaGithub],
-        image: images.mksSistemas,
-        deploy: "https://mks-test-sigma.vercel.app/",
-        repository: "https://github.com/rogervalentim/mks-test"
-    },
-    {
-        id: 5,
-        name: "carHub",
-        description: "Carhub: Uma plataforma revolucionária que oferece uma experiência de usuário excepcionalmente intuitiva. Aqui, os entusiastas de automóveis podem mergulhar em um vasto universo de opções de veículos, desde clássicos vintage até os mais modernos carros elétricos.",
-        technologies: [images.SiNextdotjs, images.BiLogoTailwindCss, images.FaGithub],
-        image: images.carHub,
-        deploy: "https://cars-lime-iota.vercel.app/",
-        repository: "https://github.com/rogervalentim/cars"
-    },
-    {
-        id: 6,
-        name: "gym",
-        description: "Esta é uma landing page simples e moderna para uma academia fictícia chamada Evogym evolutionary fitness. A página foi criada com o objetivo de atrair novos membros, apresentar as facilidades e serviços oferecidos pela academia, e promover um estilo de vida saudável e ativo.",
-        technologies: [images.FaReact, images.BiLogoTailwindCss, images.FaGithub],
-        image: images.gym,
-        deploy: "https://gym-app-mocha.vercel.app/",
-        repository: "https://github.com/rogervalentim/gym_app"
-    }
-]
+  {
+    id: 1,
+    name: "Roger cine",
+    description:
+      "O Roger Cine é um portal interativo projetado para facilitar a descoberta de filmes, séries e informações sobre atores. Com uma interface intuitiva e recursos de busca avançados, os usuários podem encontrar facilmente o conteúdo de entretenimento que amam.",
+    technologies: [
+      images.FaReact,
+      images.BiLogoTailwindCss,
+      images.BiLogoTypescript,
+      images.FaGithub
+    ],
+    image: images.rogerCine,
+    deploy: "https://roger-cines.netlify.app/",
+    repository: "https://github.com/rogervalentim/movies"
+  },
+  {
+    id: 2,
+    name: "Ifood",
+    description:
+      "O Mks Sistemas é um projeto técnico que demonstra a implementação de um carrinho de compras interativo e moderno. Utilizando as melhores práticas e tecnologias atuais, o site oferece uma experiência de usuário otimizada e um fluxo de compra simplificado.",
+    technologies: [
+      images.SiNextdotjs,
+      images.BiLogoTailwindCss,
+      images.BiLogoTypescript,
+      images.SiPrisma,
+      images.FaGithub
+    ],
+    image: images.ifood,
+    deploy: "https://ifood-fsw.vercel.app/",
+    repository: "https://github.com/rogervalentim/ifood-fsw"
+  },
+  {
+    id: 3,
+    name: "Auth Portal",
+    description:
+      "Este projeto consiste em um sistema de autenticação de usuários com três telas principais: Registro, Autenticação e Dashboard. O sistema se comunica com uma API para realizar as operações necessárias de registro, autenticação, exibição de usuários e logout.",
+    technologies: [
+      images.FaReact,
+      images.BiLogoTailwindCss,
+      images.BiLogoTypescript,
+      images.SiReactquery,
+      images.SiReacthookform,
+      images.SiZod,
+      images.FaGithub
+    ],
+    image: images.auth,
+    deploy: "https://auth-portal-taupe.vercel.app/",
+    repository: "https://github.com/rogervalentim/auth-portal"
+  },
+  {
+    id: 4,
+    name: "Github Profile",
+    description:
+      "O desafio Github Profile do Dev Challenges consiste em recriar o design fornecido e integrar com a API do GitHub para buscar e exibir informações detalhadas de usuários do GitHub. O objetivo é replicar a interface com precisão e funcionalidade, mostrando dados como repositórios, seguidores, e outras informações do perfil do usuário.",
+    technologies: [
+      images.FaReact,
+      images.BiLogoTailwindCss,
+      images.BiLogoTypescript,
+      images.FaGithub
+    ],
+    image: images.github,
+    deploy: "https://github-profile-one-delta.vercel.app/",
+    repository: "https://github.com/rogervalentim/github-profile"
+  },
+  {
+    id: 5,
+    name: "Mks Sistemas",
+    description:
+      "O Mks Sistemas é um projeto técnico que demonstra a implementação de um carrinho de compras interativo e moderno. Utilizando as melhores práticas e tecnologias atuais, o site oferece uma experiência de usuário otimizada e um fluxo de compra simplificado.",
+    technologies: [
+      images.SiNextdotjs,
+      images.SiStyledcomponents,
+      images.BiLogoTypescript,
+      images.SiJest,
+      images.FaGithub
+    ],
+    image: images.mksSistemas,
+    deploy: "https://mks-test-sigma.vercel.app/",
+    repository: "https://github.com/rogervalentim/mks-test"
+  },
+  {
+    id: 6,
+    name: "gym",
+    description:
+      "Esta é uma landing page simples e moderna para uma academia fictícia chamada Evogym evolutionary fitness. A página foi criada com o objetivo de atrair novos membros, apresentar as facilidades e serviços oferecidos pela academia, e promover um estilo de vida saudável e ativo.",
+    technologies: [images.FaReact, images.BiLogoTailwindCss, images.FaGithub],
+    image: images.gym,
+    deploy: "https://gym-app-mocha.vercel.app/",
+    repository: "https://github.com/rogervalentim/gym_app"
+  }
+];
 
 export const certificatesData = [
-    {
-        id: 1,
-        image: images.certificate1
-    },
-    {
-        id: 2,
-        image: images.certificate2
-    },
-    {
-        id: 3,
-        image: images.certificate3
-    },
-    {
-        id: 4,
-        image: images.certificate4
-    },
-    {
-        id: 5,
-        image: images.certificate5
-    },
-]
+  {
+    id: 1,
+    image: images.certificate1
+  },
+  {
+    id: 2,
+    image: images.certificate2
+  },
+  {
+    id: 3,
+    image: images.certificate3
+  },
+  {
+    id: 4,
+    image: images.certificate4
+  },
+  {
+    id: 5,
+    image: images.certificate5
+  }
+];
